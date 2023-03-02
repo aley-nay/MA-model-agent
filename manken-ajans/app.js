b6edd192f6435075a96c7a3ad3ce5b33c002298e
aley-nay <38936568+aley-nay@users.noreply.github.com>
2023-03-02 04:16:17 +0300

    Add files via upload

---
+++ manken-ajans/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+            parent: document.querySelector(".distortion"),
+            intensity: 0.2,
+            image1: "img/bw001.png",
+            image2: "img/bw002.png",
+            displacementImage: "img/effect.png"
+        });
